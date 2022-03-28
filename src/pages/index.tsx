2d38d1b0a57bb1473f6fa8a19c4ada42a9077acd
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-03-28 19:11:29 -0300

    feat(signin): signin context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,36 @@
+import styles from '../styles/Home.module.css'
+
+import { FormEvent, useContext, useState } from "react"
+
+import { AuthContext } from '../../contexts/AuthContext'
 
 export default function Home() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+
+    event.preventDefault()
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+    
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div className={styles.page}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   )
 }
