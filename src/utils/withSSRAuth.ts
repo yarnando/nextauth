71b0161d5b14655cbf4efb60f6bd885d3df6f68f
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-04-01 17:36:33 -0300

    feat: checking permissions on server side

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -2,14 +2,23 @@ import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult
 import { destroyCookie, parseCookies } from "nookies";
 import { AuthTokenError } from "../services/errors/authTokenError";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+import decode from "jwt-decode";
+import validateUserPermissions from "./validateUserPermissions";
+
+type WithSSRAuthOptions = {
+    permissions?: string[];
+    roles?: string[];
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions): GetServerSideProps {
 
     // o Next espera que a serversideprops retorne uma função, lá a gente está só chamando uma função. Portanto, temos que retornar OUTRA função, como resultado da função chamada
     return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
 
         const cookies = parseCookies(ctx);
+        const token = cookies['nextauth.token']
 
-        if (!cookies['nextauth.token']) {
+        if (!token) {
             return {
                 redirect: {
                     destination: '/',
@@ -18,11 +27,32 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
             }
         }
 
+        if(options) {
+            const user = decode<{ permissions: string[], roles: string[] }>(token)
+            const { permissions, roles } = options
+    
+            const userHasValidPermissions = validateUserPermissions({
+                user,
+                permissions,
+                roles
+            }) 
+
+            if(!userHasValidPermissions) {
+                return {
+                    redirect: {
+                        destination: '/dashboard',
+                        permanent: false,
+                    }
+                    // notFound: true, => se n tiver pagina aberta pra qqr permissao, pode jogar um notfound
+                }
+            }
+        }
+
         try {
             return await fn(ctx) //essa 'fn' é a função que tá la no serversideprops
         } catch (err) {
 
-            if (err instanceof AuthTokenError || !err?.response?.status) {
+            if (err instanceof AuthTokenError) {
                 destroyCookie(ctx, 'nextauth.token')
                 destroyCookie(ctx, 'nextauth.refreshToken')
 
